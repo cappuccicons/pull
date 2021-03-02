212424f281ea04fd7c57e9df8c7ef6b2cf3ce073
cappuccicons <79797813+cappuccicons@users.noreply.github.com>
2021-03-02 10:11:30 +0000

    Create cappuccicons.js

---
+++ cappuccicons.js
@@ -0,0 +1,1 @@
+var head,link,cssId="Cappuccicons";document.getElementById(cssId)||(head=document.getElementsByTagName("head")[0],(link=document.createElement("link")).id=cssId,link.rel="stylesheet",link.type="text/css",link.href="//cappuccicons.github.io/icons/cappuccicons.css",link.media="all",head.appendChild(link));
