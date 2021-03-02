90ac028d38dabc4040cc04722d306e65c78b9ad0
cappuccicons <79797813+cappuccicons@users.noreply.github.com>
2021-03-02 11:02:30 +0000

    Update cpf.js

--- cpf.js
+++ cpf.js
@@ -1,1 +1,3 @@
+$(document).ready(function(){
 var head,link,cssId="Cappuccicons";document.getElementById(cssId)||(head=document.getElementsByTagName("head")[0],(link=document.createElement("link")).id=cssId,link.rel="stylesheet",link.type="text/css",link.href="//icons.cappuccicons.com/cappuccicons.css",link.media="all",head.appendChild(link));
+});
