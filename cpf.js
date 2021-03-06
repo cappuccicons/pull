ffd35a50260a5d1d9f2166c2da1ad3feeaa31cfb
cappuccicons <79797813+cappuccicons@users.noreply.github.com>
2021-03-06 11:16:41 +0000

    Update cpf.js

--- cpf.js
+++ cpf.js
@@ -1,2 +1,2 @@
 var cssId='Cappuccicons';if(!document.getElementById(cssId))
-{var head=document.getElementsByTagName('head')[0];var link=document.createElement('link');link.id=cssId;link.rel='stylesheet';link.type='text/css';link.href='//icons.cappuccicons.com/cappuccicons.css';link.media='all';head.appendChild(link);}
+{var head=document.getElementsByTagName('head')[0];var link=document.createElement('link');link.id=cssId;link.rel='stylesheet';link.type='text/css';link.href='//icons.cappuccicons.com/cpf.css';link.media='all';head.appendChild(link);}
