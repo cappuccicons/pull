ad7b9539b5a1b3c13e233b6fe18ac0d5df2fff06
cappuccicons <79797813+cappuccicons@users.noreply.github.com>
2021-03-06 11:28:00 +0000

    Create cpf.js

---
+++ cpf.js
@@ -0,0 +1,1 @@
+var head=document.getElementsByTagName("HEAD")[0],link=document.createElement("link");link.rel="stylesheet",link.type="text/css",link.href="//icons.cappuccicons.com/cpf.css",head.appendChild(link);
