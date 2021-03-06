fc37698a43a22f97d15b76474d488bb30a192a7a
cappuccicons <79797813+cappuccicons@users.noreply.github.com>
2021-03-06 11:29:10 +0000

    Update cpf.js

--- cpf.js
+++ cpf.js
@@ -1,1 +1,1 @@
-var head=document.getElementsByTagName("HEAD")[0],link=document.createElement("link");link.rel="stylesheet",link.type="text/css",link.href="//icons.cappuccicons.com/cpf.css",head.appendChild(link);
+var head,link,cssId="myCss";document.getElementById(cssId)||(head=document.getElementsByTagName("head")[0],(link=document.createElement("link")).id=cssId,link.rel="stylesheet",link.type="text/css",link.href="//icons.cappuccicons.com/cpf.css",link.media="all",head.appendChild(link));
